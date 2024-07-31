3bc6c684e7632634702c3afd513e157e6941f725
20812020101584 <20812020101584@eng.zu.edu.eg>
2024-07-31 17:49:14 +0300

    overriding some methods

--- MultiTenancy.API/Data/AppDbContext.cs
+++ MultiTenancy.API/Data/AppDbContext.cs
@@ -2,5 +2,28 @@
 {
     public class AppDbContext : DbContext
     {
+        public string TenantId { get; set; }
+        private readonly ITenantServices _tenantServices;
+
+        public AppDbContext(DbContextOptions options, ITenantServices tenantServices) : base(options)
+        {
+            _tenantServices = tenantServices;
+            TenantId = _tenantServices.GetTenant()!.TenantId;
+        }
+        public DbSet<Product> Products { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasQueryFilter(x => x.TenantId == TenantId);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach (var entry in ChangeTracker.Entries<IMustHaveTenant>().Where(x => x.State == EntityState.Added))
+            {
+                entry.Entity.TenantId = TenantId;
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
     }
 }
