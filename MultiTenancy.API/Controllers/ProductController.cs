1e2c128826eff2ca27d013de976d01b631f98550
20812020101584 <20812020101584@eng.zu.edu.eg>
2024-07-31 18:28:35 +0300

    add controller

---
+++ MultiTenancy.API/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiTenancy.API.DTOS;
+
+namespace MultiTenancy.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController(IProductService productService) : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var products = await productService.GetAllAsync();
+            return Ok(products);
+        }
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var product = await productService.GetByIdAsync(id);
+
+            return product is null ? NotFound() : Ok(product);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreatedAsync(CreateProduct dto)
+        {
+            Product product = new()
+            {
+                Name = dto.Name,
+                Descreption = dto.Description,
+                Rate = dto.Rate,
+            };
+            var createdProduct = await productService.CreatedAsync(product);
+            return Ok(createdProduct);
+        }
+    }
+}
