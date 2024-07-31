06da69b0e2234e06bedbbfd663232ff3c3c38902
20812020101584 <20812020101584@eng.zu.edu.eg>
2024-07-31 17:30:29 +0300

    add services

--- MultiTenancy.API/Program.cs
+++ MultiTenancy.API/Program.cs
@@ -1,8 +1,12 @@
 
+using MultiTenancy.API.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.Configure<TenantSettings>(builder.Configuration.GetSection("TenantSettings"));
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddScoped<ITenantServices, TenantServices>();
 TenantSettings options = new();
 builder.Configuration.GetSection(nameof(TenantSettings)).Bind(options);
 builder.Services.AddControllers();
--- MultiTenancy.API/Services/TenantServices.cs
+++ MultiTenancy.API/Services/TenantServices.cs
@@ -1,6 +1,56 @@
-﻿namespace MultiTenancy.API.Services
+﻿using Microsoft.Extensions.Options;
+
+namespace MultiTenancy.API.Services
 {
-    public class TenantServices
+    public class TenantServices : ITenantServices
     {
+        private readonly IOptions<TenantSettings> _tenantSettings;
+        private Tenant? _currentTenant;
+        private HttpContext? _httpContext;
+
+        public TenantServices(IOptions<TenantSettings> tenantSettings, IHttpContextAccessor httpContextAccessor)
+        {
+            _tenantSettings = tenantSettings;
+            _httpContext = httpContextAccessor.HttpContext;
+            if (_currentTenant is not null)
+            {
+                if (_httpContext.Request.Headers.TryGetValue("tenant", out var tenantId))
+                {
+                    SetCurrentTenant(tenantId!);
+                }
+                else
+                {
+                    throw new Exception("Tenant not Provider");
+                }
+            }
+        }
+
+        public string GetDatabaseProvider()
+        {
+            return _tenantSettings.Value.Defaults.DbProvider;
+        }
+
+        public string GetConnectionString()
+        {
+            return _currentTenant.ConnectionString ?? _tenantSettings.Value.Defaults.ConnectionString;
+        }
+
+        public Tenant GetTenant()
+        {
+            return _currentTenant;
+        }
+        private void SetCurrentTenant(string tenantId)
+        {
+            _currentTenant = _tenantSettings.Value.Tenants.FirstOrDefault(x => x.TenantId == tenantId);
+            if (_currentTenant is null)
+            {
+                throw new Exception("Tenant not found");
+            }
+            if (string.IsNullOrEmpty(_currentTenant.ConnectionString))
+            {
+                _currentTenant.ConnectionString = _tenantSettings.Value.Defaults.ConnectionString;
+
+            }
+        }
     }
 }
