1b6373bd3dd3a24da2c0cfb8b66d3ad1fa9379b7
20812020101584 <20812020101584@eng.zu.edu.eg>
2024-07-31 17:04:23 +0300

    Apply DI

--- MultiTenancy.API/Program.cs
+++ MultiTenancy.API/Program.cs
@@ -1,9 +1,10 @@
+using MultiTenancy.API.Settings;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.Configure<TenantSettings>(builder.Configuration.GetSection("TenantSettings"));
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
